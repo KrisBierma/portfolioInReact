ff855f542e25a5d8ea6755ee1de86b97d42ed63a
Kris <kris.bierma@gmail.com>
2020-01-06 19:16:06 -0600

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Kris Acker Bierma's Portfolio
-
-## Build With
-
-* [React](https://reactjs.org/) - Creates UI components
-* React goodies: React easy chart, React table and Reactstrap
-* [GitHub Pages](https://pages.github.com/) - Deployment
---
+++ precache-manifest.7723e22aa54d14c895164331ac9ba77c.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8710079cf17a69e063ec1c33bc1dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0d0ad526558febc4a7",
+    "url": "/static/css/main.fcb7765a.chunk.css"
+  },
+  {
+    "revision": "4e67264e9b9fdb0f3779",
+    "url": "/static/js/2.6fc4c73f.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/static/js/2.6fc4c73f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc0d0ad526558febc4a7",
+    "url": "/static/js/main.e2fb4de3.chunk.js"
+  },
+  {
+    "revision": "aa9cd3caf36c26d4f89c",
+    "url": "/static/js/runtime-main.7a9ef57c.js"
+  },
+  {
+    "revision": "5258d21ee9562ee085d257d8b9aed61a",
+    "url": "/static/media/bamazon.5258d21e.jpg"
+  },
+  {
+    "revision": "93d11a3fc5b158100e673c3ffa09ecde",
+    "url": "/static/media/bootstrapIcon.93d11a3f.png"
+  },
+  {
+    "revision": "35bf5483f98e76b72f14dceef926e1fb",
+    "url": "/static/media/bootstrapIcon2.35bf5483.png"
+  },
+  {
+    "revision": "5de8a74636c2c3b0c395da1bf85445cc",
+    "url": "/static/media/cartoon-wars.5de8a746.png"
+  },
+  {
+    "revision": "a0143799c01b0737021c71badc23be0a",
+    "url": "/static/media/command-line.a0143799.png"
+  },
+  {
+    "revision": "bee53a59a415e920e62d71520666134c",
+    "url": "/static/media/cpp.bee53a59.png"
+  },
+  {
+    "revision": "7a3c1ce6a72b5b7348b60a164353e3f7",
+    "url": "/static/media/crystals-game.7a3c1ce6.png"
+  },
+  {
+    "revision": "0475d934d0dc2b47c3c287b8218ab5a7",
+    "url": "/static/media/firebaseIcon.0475d934.png"
+  },
+  {
+    "revision": "65c7bf1b0aa913512c7f2ca53eae2993",
+    "url": "/static/media/friendfinder.65c7bf1b.png"
+  },
+  {
+    "revision": "706ce94b5d4e1fde6e59132ece397517",
+    "url": "/static/media/gif-api.706ce94b.png"
+  },
+  {
+    "revision": "706e5e75b7cd36833e8682e2b906de33",
+    "url": "/static/media/hangman-game.706e5e75.jpg"
+  },
+  {
+    "revision": "3b84a486e222126416f68d348192463d",
+    "url": "/static/media/hbsIcon2.3b84a486.png"
+  },
+  {
+    "revision": "e0f850ead5e095cdc46a53905d777946",
+    "url": "/static/media/jqueryIcon.e0f850ea.png"
+  },
+  {
+    "revision": "c1fe2978f3f659a59e88f097e1fe563c",
+    "url": "/static/media/mongoIcon.c1fe2978.png"
+  },
+  {
+    "revision": "4bfced43bad370a8831c7dad455d5216",
+    "url": "/static/media/mongoScraper.4bfced43.jpg"
+  },
+  {
+    "revision": "e460432faf1af2aa05a599e5da7ab61a",
+    "url": "/static/media/myVeggieEats.e460432f.png"
+  },
+  {
+    "revision": "859943c64603e2265f652930c7906d16",
+    "url": "/static/media/mysqlIcon.859943c6.png"
+  },
+  {
+    "revision": "1fea17d555d6ac4d4fec1bc0c360d522",
+    "url": "/static/media/node.1fea17d5.png"
+  },
+  {
+    "revision": "f1bdaa67606d56be382ed465ab65affe",
+    "url": "/static/media/node2.f1bdaa67.png"
+  },
+  {
+    "revision": "acdb19761ea384647da9b613df482d93",
+    "url": "/static/media/node3.acdb1976.png"
+  },
+  {
+    "revision": "b4589cf0c500896fec5393050c919ba1",
+    "url": "/static/media/node4.b4589cf0.png"
+  },
+  {
+    "revision": "76012d045821d7c0c5afe413e7117e1f",
+    "url": "/static/media/node5.76012d04.png"
+  },
+  {
+    "revision": "c2d6b04939c331131e0cb720446184a4",
+    "url": "/static/media/node6.c2d6b049.png"
+  },
+  {
+    "revision": "ace52584c48291a60ea7f3363273cd63",
+    "url": "/static/media/node7.ace52584.png"
+  },
+  {
+    "revision": "9be45e9ab2d5eadfa65e8eeeb47be037",
+    "url": "/static/media/node8.9be45e9a.png"
+  },
+  {
+    "revision": "f7337d339216d05c1551688efb13a830",
+    "url": "/static/media/nodeIcon.f7337d33.png"
+  },
+  {
+    "revision": "1b3e106b3440dd3418566c703a50f516",
+    "url": "/static/media/p3.1b3e106b.jpg"
+  },
+  {
+    "revision": "0776b0e64456774847dd51c4f3af42c5",
+    "url": "/static/media/plantos.0776b0e6.jpg"
+  },
+  {
+    "revision": "1af527502f40e07fe5826d40ffadaac5",
+    "url": "/static/media/portfolio.1af52750.png"
+  },
+  {
+    "revision": "af5edaff12d63a6f17750df6460124fb",
+    "url": "/static/media/profilePic.af5edaff.JPG"
+  },
+  {
+    "revision": "f141d0104ce62ce2eb2464f615568075",
+    "url": "/static/media/profilePic2.f141d010.jpg"
+  },
+  {
+    "revision": "3b462f40f6ef1896f344e6f47de4ad3c",
+    "url": "/static/media/psalms.3b462f40.png"
+  },
+  {
+    "revision": "0ee99d4cfab96921e2a1a444d007a9f8",
+    "url": "/static/media/reactGame.0ee99d4c.png"
+  },
+  {
+    "revision": "771425c449949fdc815e16e2f6a1ee9f",
+    "url": "/static/media/reactIcon2.771425c4.png"
+  },
+  {
+    "revision": "23d36016b9640c15f2f16e5d5bd49f04",
+    "url": "/static/media/stockMarket.23d36016.png"
+  },
+  {
+    "revision": "fcb1692c32e9335e653f644cfb13d144",
+    "url": "/static/media/symphony.fcb1692c.png"
+  },
+  {
+    "revision": "a1bc9c304874a4838d4ec96674f2c9bf",
+    "url": "/static/media/train.a1bc9c30.jpg"
+  },
+  {
+    "revision": "a2a66b660361d65c6b36cced41dfff6a",
+    "url": "/static/media/treasureHunt.a2a66b66.png"
+  },
+  {
+    "revision": "04b26c06d6df44513565e55836c91398",
+    "url": "/static/media/triviaGame.04b26c06.png"
+  }
+]);
